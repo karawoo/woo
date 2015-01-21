828f641cdc6f368612d0878f6b039aaf429b812e
karawoo <woo.kara@gmail.com>
2015-01-21 13:16:43 -0800

    fxn to convert strings to title case (with spaces)

---
+++ R/title_case.R
@@ -0,0 +1,22 @@
+##' Convert strings to Title Case (with spaces)
+##'
+##' @param x string or vector of strings to convert to title case
+##' 
+##' @author Kara Woo
+##'
+##' @examples
+##' title_case("this is a sentence")
+##'
+##' @references
+##' \url{http://stackoverflow.com/a/11672354/2921990}
+##' 
+##' @export
+
+title_case <- function(x){
+  capword <- function(x) paste0(toupper(substring(x, 1, 1)),
+                                tolower(substring(x, 2, nchar(x))))
+  ifelse(is.na(x), NA,
+         sapply(strsplit(x, "\\s"), function(x) {
+           paste(capword(x), collapse = " "))
+         })
+}
